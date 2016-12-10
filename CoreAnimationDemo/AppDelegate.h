ba9ca6bf9c9b126fad064e1d816763e306de322a
王灿辉 <1361432672@qq.com>
2016-12-10 17:07:07 +0800

    Initial Commit

---
+++ CoreAnimationDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreAnimationDemo
+//
+//  Created by 王灿辉 on 2016/12/10.
+//  Copyright © 2016年 王灿辉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
